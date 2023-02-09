bb44bef138407d478b85bf350fc3c6136e851a22
tdubsxc <trdubs90@gmail.com>
2023-02-08 20:32:39 -0500

    Setup the scroll fetch functionality.

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ const imgWrapper = document.querySelector("#img-wrapper");
 const loaderEl = document.querySelector("#loader");
 
 const accessKey = "MAG6ovfk7IvFVd8rHKAOY2m9C0X3YGVUFPQW8LubeN0";
-const count = 5;
+const count = 10;
 const endpoint = `https://api.unsplash.com/photos/random/?client_id=${accessKey}&count=${count}&orientation=landscape`;
 
 let query = "";
+let isReady = false;
+let imageLoadedCount = 0;
+let totalImages = 0;
 
 form.addEventListener("submit", handleSubmit);
+window.addEventListener("scroll", handleScroll);
 
 async function handleSubmit(e) {
   e.preventDefault();
+  imgWrapper.innerHTML = null;
 
   query = form["query"].value;
 
@@ -22,6 +27,13 @@ async function handleSubmit(e) {
 
   fetchPhotos(endpoint, query);
   form.reset();
+}
+
+function handleScroll(_e) {
+  if (window.scrollY + window.innerHeight >= document.body.offsetHeight - 1000 && isReady) {
+    isReady = false;
+    fetchPhotos(endpoint, query);
+  }
 }
 
 async function fetchPhotos(endpoint, query) {
@@ -40,6 +52,9 @@ async function fetchPhotos(endpoint, query) {
 }
 
 function displayPhotos(photos) {
+  imageLoadedCount = 0;
+  totalImages = photos.length;
+
   photos.forEach((photo) => {
     const aTag = document.createElement("a");
     setAttrs(aTag, { href: photo.links.html, target: "_blank" });
@@ -47,9 +62,19 @@ function displayPhotos(photos) {
     const imgTag = document.createElement("img");
     setAttrs(imgTag, { src: photo.urls.regular, alt: photo.alt_description, title: photo.alt_description });
 
+    imgTag.addEventListener("load", imageLoaded);
+
     aTag.append(imgTag);
     imgWrapper.append(aTag);
   });
+}
+
+function imageLoaded() {
+  imageLoadedCount += 1;
+
+  if (imageLoadedCount === totalImages) {
+    isReady = true;
+  }
 }
 
 function setAttrs(el, attrs) {

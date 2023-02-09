8d55d4f9346bb45b05f889c2327bae7b344a9e99
tdubsxc <trdubs90@gmail.com>
2023-02-08 20:00:47 -0500

    Tweaked the fetch request and the form submit function.

--- app.js
+++ app.js
@@ -2,56 +2,58 @@ const form = document.querySelector("#form");
 const imgWrapper = document.querySelector("#img-wrapper");
 const loaderEl = document.querySelector("#loader");
 
-const accessKey = "mpl3SQiZ8Vi_aU1wcCnF93dkbF3FknJUUe_KvnJHcLA";
+const accessKey = "MAG6ovfk7IvFVd8rHKAOY2m9C0X3YGVUFPQW8LubeN0";
 const count = 5;
-const query = "architecture";
 const endpoint = `https://api.unsplash.com/photos/random/?client_id=${accessKey}&count=${count}&orientation=landscape`;
+
+let query = "";
 
 form.addEventListener("submit", handleSubmit);
 
 async function handleSubmit(e) {
   e.preventDefault();
 
-  const query = form["query"].value;
+  query = form["query"].value;
 
   if (!query.trim()) {
     alert("Please enter a search term!");
     return;
   }
 
-  try {
-    const photos = await fetcher(`${endpoint}&query=${query}`);
-
-    if (photos.errors) throw new Error("Invalid search... please try again");
-
-    displayPhotos(photos);
-  } catch (error) {
-    alert(error.message);
-  }
-
+  fetchPhotos(endpoint, query);
   form.reset();
 }
 
-function displayPhotos(photos) {
-  imgWrapper.innerHTML = null;
+async function fetchPhotos(endpoint, query) {
+  try {
+    const res = await fetch(`${endpoint}&query=${query}`);
+    const data = await res.json();
 
+    if (data.errors) {
+      throw new Error("Invalid search... please try again");
+    }
+
+    displayPhotos(data);
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+function displayPhotos(photos) {
   photos.forEach((photo) => {
     const aTag = document.createElement("a");
-    aTag.setAttribute("href", photo.url);
-    aTag.setAttribute("target", "_blank");
+    setAttrs(aTag, { href: photo.links.html, target: "_blank" });
 
     const imgTag = document.createElement("img");
-    imgTag.src = photo.urls.regular;
-    imgTag.alt = photo.alt_description;
-    imgTag.title = photo.alt_description;
+    setAttrs(imgTag, { src: photo.urls.regular, alt: photo.alt_description, title: photo.alt_description });
 
     aTag.append(imgTag);
     imgWrapper.append(aTag);
   });
 }
 
-async function fetcher(url) {
-  const res = await fetch(url);
-  const data = await res.json();
-  return data;
+function setAttrs(el, attrs) {
+  for (const prop in attrs) {
+    el.setAttribute(prop, attrs[prop]);
+  }
 }
